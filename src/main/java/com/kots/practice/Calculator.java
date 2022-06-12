4be54c023b555ee2306bd947f17934529985c5f2
Artem Kot <artem.alex.kot@gmail.com>
2022-06-12 19:21:14 +0400

    Initial commit

---
+++ src/main/java/com/kots/practice/Calculator.java
@@ -0,0 +1,7 @@
+package com.kots.practice;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a+b;
+    }
+}
---
+++ src/test/java/com/kots/practice/CalculatorTest.java
@@ -0,0 +1,43 @@
+/**
+ * Parameterized test for a simple calculator class
+ */
+
+package com.kots.practice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class CalculatorTest {
+    private final int number1;
+    private final int number2;
+    private final int expected;
+
+    public CalculatorTest(int number1, int number2, int expected) {
+        this.number1 = number1;
+        this.number2 = number2;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Object[] getTestData() {
+        return new Object[][] {
+                {-6,-1,-7},
+                {-1,0,-1},
+                {0,0,0},
+                {-1,1,0},
+                {-1,2,1},
+                {1,1,2},
+                {3,-2,1}
+        };
+    }
+    @Test
+    public void calTest() {
+        Calculator calculator = new Calculator();
+        int actual = calculator.sum(number1, number2);
+        assertEquals(expected, actual);
+    }
+}
