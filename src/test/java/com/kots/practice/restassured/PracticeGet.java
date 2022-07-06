78419d32d02d74f627136bfae4b25939bc5d4438
artem-kot <artem.alex.kot@gmail.com>
2022-07-06 17:56:03 +0400

    topic: API. Parent abstract class is created to store token. Refactored accordingly.

--- src/test/java/com/kots/practice/restassured/PracticeGet.java
+++ src/test/java/com/kots/practice/restassured/PracticeGet.java
@@ -6,18 +6,13 @@ import org.junit.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 
-public class PracticeGet {
-
-    @Before
-    public void setup() {
-        baseURI = "https://qa-mesto.praktikum-services.ru";
-    }
+public class PracticeGet extends BaseTestData{
 
     @Test
     public void testGetUserInfo() {
         given()
                 .auth()
-                .oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU0MmU3ZWQzYjg2YTAwM2Q2N2MyNjkiLCJpYXQiOjE2NTY0ODkyODgsImV4cCI6MTY1NzA5NDA4OH0.dOE0IGs3ULyA810CdSp6ug6SxuGDCYXoDMb8XHvuYD0")
+                .oauth2(token)
                 .get("/api/users/me")
                 .then().statusCode(200)
                 .assertThat().body("data.name", equalTo("Аристарх Сократович"));
@@ -26,14 +21,14 @@ public class PracticeGet {
     @Test
     public void testGetCards() {
         given()
-                .auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU0MmU3ZWQzYjg2YTAwM2Q2N2MyNjkiLCJpYXQiOjE2NTY0ODkyODgsImV4cCI6MTY1NzA5NDA4OH0.dOE0IGs3ULyA810CdSp6ug6SxuGDCYXoDMb8XHvuYD0")
+                .auth().oauth2(token)
                 .get("/api/cards")
                 .then().statusCode(200);
     }
 
     @Test
     public void testUserOccupation() {
-        Response response = given().auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU0MmU3ZWQzYjg2YTAwM2Q2N2MyNjkiLCJpYXQiOjE2NTY0ODkyODgsImV4cCI6MTY1NzA5NDA4OH0.dOE0IGs3ULyA810CdSp6ug6SxuGDCYXoDMb8XHvuYD0").get("/api/users/me");
+        Response response = given().auth().oauth2(token).get("/api/users/me");
         response.then().assertThat().body("data.about", equalTo("Автор автотестов"));
     }
 }
