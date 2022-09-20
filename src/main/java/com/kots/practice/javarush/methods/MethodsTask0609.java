cacc63052ce2183da11002ab87cd5ae20a420512
Artem Kot <artem.alex.kot@gmail.com>
2022-09-20 19:59:19 +0400

    Javarush task 0609 is passed.

---
+++ src/main/java/com/kots/practice/javarush/methods/MethodsTask0609.java
@@ -0,0 +1,30 @@
+package com.kots.practice.javarush.methods;
+
+public class MethodsTask0609 {
+    public static void main(String[] args) {
+        System.out.println("Task 0609");
+        /**
+         * Create a method which will return 9th mathematical degree of a long number.
+         */
+        System.out.println(cube(2));
+        System.out.println(ninthDegree(2));
+        System.out.println(stepen(2,9));
+
+    }
+
+    public static long cube(long a){
+        return a*a*a;
+    }
+
+    public static long ninthDegree(long a){
+        return cube(cube(a));
+    }
+
+    public static long stepen(long a, int degree){
+        long b = a;
+        for(int i = 1; i < degree; i++){
+            b = b * a;
+        }
+        return b;
+    }
+}
