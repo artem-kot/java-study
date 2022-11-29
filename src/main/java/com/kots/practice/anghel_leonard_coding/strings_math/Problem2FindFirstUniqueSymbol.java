dbbbb911721193d1173a4b8c3ad3e0a98b48ef9f
artem-kot <artem.alex.kot@gmail.com>
2022-11-29 11:24:17 +0100

    Anghel Leonard coding problems book. Task 2 is completed.

--- src/main/java/com/kots/practice/anghel_leonard_coding/strings_math/Problem2FindFirstUniqueSymbol.java
+++ src/main/java/com/kots/practice/anghel_leonard_coding/strings_math/Problem2FindFirstUniqueSymbol.java
@@ -8,25 +8,30 @@ package com.kots.practice.anghel_leonard_coding.strings_math;
 public class Problem2FindFirstUniqueSymbol {
 
     private final String string = "We're going to visit a Christian Löffler's concert";
-    private final String string2 = "Hello, home";
 
     public char firstUniqueSymbol(String string){
         String input = string.toLowerCase();
-        char result = 0;
-        for (int i = 0; i < input.length(); i++){ //TODO: continue fixing this method. Find the right algorithm.
-            char symbol = string.charAt(i);
-            for(int j = i+1; j < string.length(); j++){
-                if (string.charAt(j) == symbol){
-                    break;
-                }
-            } result = symbol;
-        } return result;
+        for (int i = 0; i < input.length(); i++){
+            if (isCharUnique(input, i)){
+                return input.charAt(i);
+            }
+        }
+        return 0;
     }
 
+    public boolean isCharUnique(String input, int charIndex){
+        for (int i = 0; i < input.length(); i++){
+            if (i!=charIndex && input.charAt(charIndex) == input.charAt(i)){
+                break;
+            } else if (i!= charIndex && i == input.length()-1) {
+                return true;
+            }
+        } return false;
+    }
 
     public static void main(String[] args) {
         Problem2FindFirstUniqueSymbol task = new Problem2FindFirstUniqueSymbol();
-        char result = task.firstUniqueSymbol(task.string2);
+        char result = task.firstUniqueSymbol(task.string);
         System.out.println(result);
     }
 }
