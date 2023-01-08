675c61a089706110fc01e19572a04b4b85ba4ce2
artem-kot <artem.alex.kot@gmail.com>
2023-01-08 23:09:08 +0100

    Javarush collections.

---
+++ src/main/java/com/kots/practice/javarush/collections/arraylist/ArrayListTask1209.java
@@ -0,0 +1,51 @@
+package com.kots.practice.javarush.collections.arraylist;
+
+// В классе Solution есть список сотрудников waitingEmployees, которые ждут зарплату,
+// и список сотрудников alreadyGotSalaryEmployees, которые её уже получили.
+// Необходимо реализовать метод paySalary(String), который в качестве аргумента принимает
+// имя сотрудника, желающего получить зарплату.
+//
+// Метод должен проверить наличие сотрудника в списке waitingEmployees, и если он не получал зарплату, выдать её:
+// Если параметр метода paySalary(String) равняется null, ничего не нужно делать.
+// Добавить имя сотрудника в список alreadyGotSalaryEmployees.
+// В списке waitingEmployees заменить имя сотрудника на null.
+
+//	Метод paySalary(String) должен добавлять в список alreadyGotSalaryEmployees имя сотрудника, если он есть в списке waitingEmployees.
+//•	Метод paySalary(String) должен заменять имя сотрудника на значение null, если он есть в списке waitingEmployees.
+//•	Метод paySalary(String) не должен изменять списки waitingEmployees и alreadyGotSalaryEmployees, если переданного имени нет в списке waitingEmployees.
+//•	Если в метод paySalary(String) передается null, метод не должен добавлять это значение в waitingEmployees или alreadyGotSalaryEmployees.
+
+import java.util.ArrayList;
+
+public class ArrayListTask1209 {
+    public static ArrayList<String> waitingEmployees = new ArrayList<>();
+    public static ArrayList<String> alreadyGotSalaryEmployees = new ArrayList<>();
+
+    public static void initEmployees() {
+        waitingEmployees.add("Гвинно");
+        waitingEmployees.add("Гунигерд");
+        waitingEmployees.add("Боргелейф");
+        waitingEmployees.add("Нифрод");
+        waitingEmployees.add("Альбиуф");
+        waitingEmployees.add("Иногрим");
+        waitingEmployees.add("Фриле");
+    }
+
+    public static void main(String[] args) {
+        initEmployees();
+        for(int i = 0; i < waitingEmployees.size(); i++){
+            paySalary(waitingEmployees.get(i));
+        }
+
+        for (int i = 0; i < alreadyGotSalaryEmployees.size(); i++){
+            System.out.println(waitingEmployees.get(i));
+        }
+    }
+
+    public static void paySalary(String name) {
+        if(waitingEmployees.contains(name)){
+            alreadyGotSalaryEmployees.add(name);
+            waitingEmployees.set(waitingEmployees.indexOf(name), null);
+        }
+    }
+}
