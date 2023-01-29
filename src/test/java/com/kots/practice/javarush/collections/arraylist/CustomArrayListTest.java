5cb0db0cd101a36d449c218d0ae421ba31e8b45a
artem-kot <artem.alex.kot@gmail.com>
2023-01-29 22:58:26 +0100

    Created a custom ArrayList class for javaRush task 1212.

---
+++ src/test/java/com/kots/practice/javarush/collections/arraylist/CustomArrayListTest.java
@@ -0,0 +1,29 @@
+package com.kots.practice.javarush.collections.arraylist;
+
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomArrayListTest {
+
+    private CustomArrayList prepareTestData(int size) {
+        CustomArrayList list = new CustomArrayList();
+        for (int i = 0; i < size; i++) {
+            list.add(RandomUtils.nextInt(0, 20));
+        }
+        return list;
+    }
+
+    @Test
+    public void testCustomArrayWithDefaultCapacity() {
+        CustomArrayList testList = prepareTestData(1);
+        assertEquals(10, testList.capacity);
+    }
+
+    @Test
+    public void testCustomArrayWithIncreasedCapacity() {
+        CustomArrayList testList = prepareTestData(11);
+        assertEquals(15, testList.capacity);
+    }
+}
