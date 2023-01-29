d936edda26632fe15ad63ad672f894cd3a40598a
artem-kot <artem.alex.kot@gmail.com>
2023-01-29 22:58:51 +0100

    Introduced tests for custom ArrayList from javarush 1212.

---
+++ src/main/java/com/kots/practice/javarush/collections/arraylist/CustomArrayList.java
@@ -0,0 +1,38 @@
+package com.kots.practice.javarush.collections.arraylist;
+
+import java.util.Arrays;
+
+/**
+ * A bit of theory: once declared, ArrayList by default has size = 10. However, once we add element #10 (in other
+ * words, once we filled the last empty slot of ArrayList), it's potential size (capacity) is automatically increased.
+ * It's done via creating a new ArrayList object with capacity = 1.5 of the previous one (e.g. 10 -> 15).
+ * This task is intended to visualize it better.
+ *
+ * We have to create CustomArrayList class with add() method, which implements logic described above.
+ */
+
+
+public class CustomArrayList {
+    int size;
+    int capacity;
+    Integer[] array;
+
+    public CustomArrayList() {
+        size = 0;
+        capacity = 10;
+        array = new Integer[capacity];
+    }
+
+    public void add(int number) {
+        if (size == capacity) {
+            grow();
+        }
+        array[size] = number;
+        size++;
+    }
+
+    private void grow() {
+        capacity += capacity/2;
+        array = Arrays.copyOf(array, capacity);
+    }
+}
