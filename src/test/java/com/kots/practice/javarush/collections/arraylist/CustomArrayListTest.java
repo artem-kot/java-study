293db41de6ade8cf57562df8cff839ba568a8f2b
artem-kot <artem.alex.kot@gmail.com>
2023-01-29 23:50:10 +0100

    Introduced test of an advanced add() method for custom ArrayList.

--- src/test/java/com/kots/practice/javarush/collections/arraylist/CustomArrayListTest.java
+++ src/test/java/com/kots/practice/javarush/collections/arraylist/CustomArrayListTest.java
@@ -1,12 +1,16 @@
 package com.kots.practice.javarush.collections.arraylist;
 
 import org.apache.commons.lang3.RandomUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CustomArrayListTest {
 
+    public static CustomArrayList testList;
     private CustomArrayList prepareTestData(int size) {
         CustomArrayList list = new CustomArrayList();
         for (int i = 0; i < size; i++) {
@@ -17,13 +21,23 @@ class CustomArrayListTest {
 
     @Test
     public void testCustomArrayWithDefaultCapacity() {
-        CustomArrayList testList = prepareTestData(1);
+        testList = prepareTestData(1);
         assertEquals(10, testList.capacity);
     }
 
     @Test
     public void testCustomArrayWithIncreasedCapacity() {
-        CustomArrayList testList = prepareTestData(11);
+        testList = prepareTestData(11);
         assertEquals(15, testList.capacity);
+    }
+
+    @Test
+    public void testCustomArrayWithIndexedAdd() {
+        testList = prepareTestData(10);
+        System.out.println(Arrays.toString(testList.array));
+        testList.add(5, -1);
+        System.out.println(Arrays.toString(testList.array));
+        assertEquals(11, testList.size);
+        assertEquals(-1, testList.array[5]);
     }
 }
