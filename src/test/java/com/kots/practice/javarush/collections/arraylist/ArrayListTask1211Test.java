9373991e249c756bac2f1cdbceabdea2e688a363
artem-kot <artem.alex.kot@gmail.com>
2023-01-29 22:20:30 +0100

    Created a test for javaRush task 1211.

---
+++ src/test/java/com/kots/practice/javarush/collections/arraylist/ArrayListTask1211Test.java
@@ -0,0 +1,29 @@
+package com.kots.practice.javarush.collections.arraylist;
+
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListTask1211Test {
+
+    private static final ArrayList<Integer> numbers = new ArrayList<>();
+    private ArrayList<Integer> getTestData(){
+        for(int i = 0; i < 10; i++) {
+            numbers.add(i, RandomUtils.nextInt(0, 100));
+        }
+        numbers.set(RandomUtils.nextInt(1, 11), -100);
+        System.out.println("Set negative number at position " + numbers.indexOf(-100));
+        return numbers;
+    }
+
+    @Test
+    public void bubbleSortingTest() {
+        ArrayListTask1211 task = new ArrayListTask1211();
+        Assertions.assertEquals(-100, task.sort(getTestData()).get(0));
+    }
+}
