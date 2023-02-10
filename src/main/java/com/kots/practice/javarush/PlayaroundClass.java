578b116c5c0825d9f6e932d9ef97a0f3d64cc265
artem-kot <artem.alex.kot@gmail.com>
2023-02-10 14:20:15 +0100

    Tested some maps caching and cleanup mechanism for work.

--- src/main/java/com/kots/practice/javarush/PlayaroundClass.java
+++ src/main/java/com/kots/practice/javarush/PlayaroundClass.java
@@ -1,8 +1,33 @@
 package com.kots.practice.javarush;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PlayaroundClass {
     public static void main(String[] args) {
         int multiplier = (int) (1 + Math.random() * 1000);
         System.out.println("Факт " + (int) ((1 + Math.random()) * multiplier) + ":\n");
+
+        Map<String, ArrayList<String>> dialogs = new HashMap<>();
+
+        dialogs.put("111", new ArrayList<>());
+        dialogs.put("222", new ArrayList<>());
+        dialogs.put("333", new ArrayList<>());
+
+        dialogs.get("111").add("Hello");
+        dialogs.get("111").add("How are you?");
+        dialogs.get("111").add("I'm here for you");
+
+        dialogs.get("333").add("Gosh I hate you");
+        dialogs.get("333").add("HATRED!");
+
+        for(String user : dialogs.keySet()) {
+            HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+            map.put(user, dialogs.get(user));
+            System.out.println(Arrays.deepToString(map.get(user).toArray()));
+        }
+
     }
 }
---
+++ src/main/java/com/kots/practice/javarush/maps/PlaygroundMapCaching.java
@@ -0,0 +1,57 @@
+package com.kots.practice.javarush.maps;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlaygroundMapCaching {
+
+    private static final Map<String, ArrayList<String>> dialogs = new HashMap<>();
+    private final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss.SS");
+
+    private void logMessage(String userId, String initiator, String request, String response) {
+        ArrayList<String> input = new ArrayList<>();
+        input.add(dtf.format(LocalTime.now()) + " >> " + initiator + " : " + request);
+        input.add(dtf.format(LocalTime.now()) + " >> Lara: " + response);
+
+        if (dialogs.containsKey(userId)) {
+            updateDialog(userId, input);
+        } else {
+            dialogs.put(userId, input);
+        }
+
+        System.out.println("Dialogs: " + dialogs.size());
+        System.out.println("Users: " + Arrays.toString(dialogs.keySet().toArray()));
+    }
+
+    private static void updateDialog(String userId, ArrayList<String> input) {
+        if (dialogs.get(userId).size() >= 8) {
+            for (int i = dialogs.get(userId).size() - 8 + input.size(); i >= 0; i--) {
+                dialogs.get(userId).remove(0);
+            }
+        }
+        for (String utterance : input) {
+            dialogs.get(userId).add(utterance);
+        }
+    }
+
+    public static void main(String[] args) {
+        PlaygroundMapCaching dialog = new PlaygroundMapCaching();
+        dialog.logMessage("1", "Artem", "ping 1", "pong 1");
+        dialog.logMessage("2", "Helen", "ping 1", "pong 1");
+        dialog.logMessage("1", "Artem", "ping 2", "pong 2");
+        dialog.logMessage("1", "Artem", "ping 3", "pong 3");
+        dialog.logMessage("1", "Artem", "ping 4", "pong 4");
+        dialog.logMessage("1", "Artem", "ping 5", "pong 5");
+
+        Gson serGson = new GsonBuilder().setPrettyPrinting().create();
+        String json = serGson.toJson(dialogs);
+        System.out.println(json);
+    }
+}
