64a05e2ab47fc28c0323c153f08acd83b3db53e1
artem-kot <artem.alex.kot@gmail.com>
2023-08-24 17:13:40 +0200

    Updated kata solution with the link to the task.

--- src/main/java/com/kots/practice/codewars/kyu7_CreditCardMask.java
+++ src/main/java/com/kots/practice/codewars/kyu7_CreditCardMask.java
@@ -1,5 +1,7 @@
 package com.kots.practice.codewars;
 
+// https://www.codewars.com/kata/5412509bd436bd33920011bc/solutions/java
+
 public class kyu7_CreditCardMask {
     public static String maskify(String str) {
         return str.length() > 4 ? "#".repeat(str.length()-4) + str.substring(str.length()-4) : str;
